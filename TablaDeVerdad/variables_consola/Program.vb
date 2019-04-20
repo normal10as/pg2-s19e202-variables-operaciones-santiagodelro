452543accc2e7032dd3b76840ee98a2e0bf39ac1
Usuario <Usuario@NENONCHI>
2019-04-20 19:21:20 -0300

    1.8

---
+++ TablaDeVerdad/variables_consola/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module TablaDeVerdad
+    Sub Main(args As String())
+        Dim P, Q, Resultado As Boolean
+
+        P = True
+        Q = False
+        Resultado = P And Q
+        Console.WriteLine("AND: " & Resultado)
+        Resultado = P Or Q
+        Console.WriteLine("OR: " & Resultado)
+        Resultado = P Xor Q
+        Console.WriteLine("XOR: " & Resultado)
+        Resultado = Not P
+        Console.WriteLine("NOT P: " & Resultado)
+        Resultado = Not Q
+        Console.WriteLine("NOT Q: " & Resultado)
+    End Sub
+End Module
