8310e42ea48b150ba9cc3d78e1dc56eb2795063e
Usuario <Usuario@NENONCHI>
2019-04-20 18:30:57 -0300

    1.6

---
+++ Sumatoria/Variables_consola/Program.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module Sumatoria
+    Sub Main(args As String())
+        Dim a, b, c, d, Suma, Resultado As Integer
+        Console.WriteLine("Ingrese 4 valores: ")
+        a = Console.ReadLine()
+        b = Console.ReadLine()
+        c = Console.ReadLine()
+        d = Console.ReadLine()
+        Suma = a + b + c + d
+        Resultado = Suma / 4
+
+        Console.WriteLine("La suma total es: " & Suma)
+        Console.WriteLine("El promedio es: " & Resultado)
+
+
+
+
+    End Sub
+End Module
