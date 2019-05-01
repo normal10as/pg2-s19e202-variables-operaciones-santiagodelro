1a45329ecb723d6cc1caafa260c3b6ebde3bcbd4
Usuario <Usuario@NENONCHI>
2019-04-30 21:49:29 -0300

    1.8

--- TablaDeVerdad/variables_consola/Program.vb
+++ TablaDeVerdad/variables_consola/Program.vb
@@ -1,20 +1,53 @@
 Imports System
 
-Module TablaDeVerdad
+Module Table_booleana
     Sub Main(args As String())
-        Dim P, Q, Resultado As Boolean
+        Dim P1, P2, Q1, Q2, R1, R2, R3, R4 As Boolean
 
-        P = 1
-        Q = 0
-        Resultado = P And Q
-        Console.WriteLine("AND: " & Resultado)
-        Resultado = P Or Q
-        Console.WriteLine("OR: " & Resultado)
-        Resultado = P Xor Q
-        Console.WriteLine("XOR: " & Resultado)
-        Resultado = Not P
-        Console.WriteLine("NOT P: " & Resultado)
-        Resultado = Not Q
-        Console.WriteLine("NOT Q: " & Resultado)
+        P1 = True
+        Q1 = False
+        P2 = False
+        Q2 = True
+        '-----------------AND------------
+        R1 = P1 And Q1
+        R2 = P2 And Q2
+        R3 = P2 And Q1
+        R4 = P1 And Q2
+
+        Console.WriteLine("-------AND-------")
+        Console.WriteLine("V    V   " & R4)
+        Console.WriteLine("V    F   " & R1)
+        Console.WriteLine("V    V   " & R2)
+        Console.WriteLine("V    V   " & R3)
+        '---------------OR--------------
+        R1 = P1 Or Q1
+        R2 = P2 Or Q2
+        R3 = P2 Or Q1
+        R4 = P1 Or Q2
+
+        Console.WriteLine("-------OR-------")
+        Console.WriteLine("V    V   " & R4)
+        Console.WriteLine("V    F   " & R1)
+        Console.WriteLine("V    V   " & R2)
+        Console.WriteLine("V    V   " & R3)
+        '---------------XOR--------------
+        R1 = P1 Xor Q1
+        R2 = P2 Xor Q2
+        R3 = P2 Xor Q1
+        R4 = P1 Xor Q2
+
+        Console.WriteLine("-------XOR-------")
+        Console.WriteLine("V    V   " & R4)
+        Console.WriteLine("V    F   " & R1)
+        Console.WriteLine("V    V   " & R2)
+        Console.WriteLine("V    V   " & R3)
+        '---------------NOT--------------
+        R1 = Not P1
+        R2 = Not Q1
+        Console.WriteLine("-------NOT-------")
+        Console.WriteLine("V        " & R1)
+        Console.WriteLine("F        " & R2)
+
+
     End Sub
 End Module
