67640c1ed5fce11d2a785c05caa57dc61a221f28
Usuario <Usuario@NENONCHI>
2019-05-01 22:12:41 -0300

    1.18

---
+++ Nueva_fecha/Nueva_fecha/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Nueva_fecha
+    Sub Main(args As String())
+        Dim dia, diasPlus, mes, anio, nuevafecha
+        Dim fecha As Date
+        Console.Write("Ingrese una fecha(dia/mes/anio): ")
+        fecha = Console.ReadLine()
+        Console.Write("Ingrese los dias a agregar: ")
+        dia = Console.ReadLine()
+        nuevafecha = fecha.AddDays(dia)
+        Console.Write("La fecha es: " & fecha & vbLf & "La nueva fecha es: " & nuevafecha)
+
+
+    End Sub
+End Module
