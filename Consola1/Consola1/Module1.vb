a96bfbe484a985295d8c4288ab00f7f7767f2796
Usuario <Usuario@NENONCHI>
2019-05-06 21:52:23 -0300

    ok

---
+++ Consola1/Consola1/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Palabra As String
+        Dim Posicion As Integer
+        Console.Write("Ingrese una palabra: ")
+        Palabra = Console.ReadLine()
+        Posicion = InStr(1, Palabra, "a")
+        Console.WriteLine(Posicion)
+    End Sub
+
+End Module
--- Semanas/variables_consola/Program.vb
+++ Semanas/variables_consola/Program.vb
@@ -3,12 +3,16 @@ Imports System
 Module equivalencia_dias
     Sub Main(args As String())
         Dim MinutosPorHora, DiasPorSemana, HorasPorDia As Single
+        Const semana As Integer = 7
+        Const hora As Integer = 24
+        Const minutos As Integer = 60
+
         Dim Dias As Integer
         Console.Write("Ingrese Los Dias: ")
         Dias = Console.ReadLine()
-        DiasPorSemana = Dias / 7
-        HorasPorDia = Dias * 24
-        MinutosPorHora = HorasPorDia * 60
+        DiasPorSemana = Dias / semana
+        HorasPorDia = Dias * hora
+        MinutosPorHora = HorasPorDia * minutos
         Console.WriteLine("Numero de Semanas: " & DiasPorSemana)
         Console.WriteLine("Numero de Horas: " & HorasPorDia)
         Console.WriteLine("Numero de minutos: " & MinutosPorHora)
