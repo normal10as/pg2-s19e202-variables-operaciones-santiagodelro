dd3cf7495c735097d50e0989374d5d06b6b91796
Usuario <Usuario@NENONCHI>
2019-05-10 17:22:13 -0300

    15

---
+++ Refrase/Refrase/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Cadena As String = "Aunque la cadena sea de oro, sirve para lo mismo."
+        Dim Palabra, PalabraNueva, Resultado As String
+        Console.WriteLine("Aunque la cadena sea de oro, sirve para lo mismo.")
+        Console.Write("Ingrese la palabra a reemplazar: ")
+        Palabra = Console.ReadLine()
+        Console.Write("Ingrese la Nueva palabra: ")
+        PalabraNueva = Console.ReadLine()
+        PalabraNueva = " " + PalabraNueva + " "
+        Resultado = Replace(Cadena, Palabra, PalabraNueva)
+        Console.WriteLine("Resultado: " & Resultado)
+
+    End Sub
+
+End Module
