abefe65e296e2588344e62c3403968c54df9c7ca
Usuario <Usuario@NENONCHI>
2019-05-10 17:05:45 -0300

    14

--- Consola1/Consola1/Module1.vb
+++ Consola1/Consola1/Module1.vb
@@ -1,12 +1,18 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim Palabra As String
+        Dim Palabra, Invertida As String
         Dim Posicion As Integer
         Console.Write("Ingrese una palabra: ")
         Palabra = Console.ReadLine()
         Posicion = InStr(1, Palabra, "a")
-        Console.WriteLine(Posicion)
+        Console.WriteLine("La primera a aparece en la posicion: " & Posicion)
+        Posicion = InStr(1, Palabra, "o")
+        Console.WriteLine("La primera o aparece en la posicion: " & Posicion)
+        Invertida = StrReverse(Palabra)
+        Console.WriteLine("La palabra invertida: " & Invertida)
+
+
     End Sub
 
 End Module
